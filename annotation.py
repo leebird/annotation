59dd60a3f9b4c2d3cf60828557f19e4249b0deba
leebird <leemagpie@gmail.com>
2014-07-26 02:40:56 -0400

    add Base class with common methods

--- annotation.py
+++ annotation.py
@@ -1,21 +1,6 @@
 # -*- coding: utf-8 -*-
 
-class Entity:    
-    def __init__(self,tid,typing,start,end,text):
-        self.id = tid
-        self.type = typing
-        self.start = start
-        self.end = end
-        self.text = text
-        self.tmpl = u'{}_{}_{}_{}_{}'
-
-    def __unicode__(self):
-        return self.tmpl.format(self.id,
-                                self.type,
-                                self.start,
-                                self.end,
-                                self.text)
-
+class Base(object):
     def __str__(self):
         return unicode(self).encode('utf-8')
 
@@ -29,35 +14,43 @@ class Entity:
             return False
 
     def __ne__(self, other):
-        return not self.__eq__(other)
+        return not self.__eq__(other)    
 
-class Event:
+class Entity(Base):
+    def __init__(self,tid,typing,start,end,text):
+        self.id = tid
+        self.type = typing
+        self.start = start
+        self.end = end
+        self.text = text
+        self.tmpl = u'{0}_{1}_{2}_{3}_{4}'
+
+    def __unicode__(self):
+        return self.tmpl.format(self.id,
+                                self.type,
+                                self.start,
+                                self.end,
+                                self.text)
+
+class Event(Base):
     def __init__(self,tid,typing,triggerId,args):
         self.id = tid
         self.type = typing
         self.triggerId = triggerId
         self.args = args
-        self.tmpl = u'{}_{}_{}_{}'
+        self.tmpl = u'{0}_{1}_{2}_{3}'
 
     def __unicode__(self):
         return self.tmpl.format(self.id,self.type,
                                 self.triggerId,self.args)
-
-    def __repr__(self):
-        return self.__unicode__()
-
-        
-class Relation:
+class Relation(Base):
     def __init__(self,rid,typing,arg1,arg2):
         self.id = rid
         self.type = typing
         self.arg1 = arg1
         self.arg2 = arg2
-        self.tmpl = u'{}_{}_{}_{}'
+        self.tmpl = u'{0}_{1}_{2}_{3}'
 
     def __unicode__(self):
         return self.tmpl.format(self.id,self.type,
                                 self.arg1,self.arg2)
-
-    def __repr__(self):
-        return self.__unicode__()
